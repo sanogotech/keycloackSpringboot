6b7411172bfa077281e7f400afd71bfdd734cbde
SANOGOTECH <sanogotech@yahoo.fr>
2022-06-02 16:02:27 +0000

    Update TokenController.java

--- src/main/java/io/akoserwa/SecureKeycloakApp/controllers/TokenController.java
+++ src/main/java/io/akoserwa/SecureKeycloakApp/controllers/TokenController.java
@@ -2,7 +2,7 @@ package io.akoserwa.SecureKeycloakApp.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
